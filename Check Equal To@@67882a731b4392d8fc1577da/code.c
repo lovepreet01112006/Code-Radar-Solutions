7383e4cd799892a8f263812061d9167a4ba170d7
lovepreet01112006 <lovepreet1873.becse24@chitkara.edu.in>
2025-01-28 09:56:35 +0530

    Solution to Check Equal To

---
+++ Check Equal To@@67882a731b4392d8fc1577da/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int x;
+    int y;
+    scanf("%d %d",&x,&y);
+    if(x==y){
+        printf("True");
+    }
+    else if(x<y && x>y){
+        printf("False");
+    }
+    else{
+        printf("Not Determined")
+    }
+    return 0;
+}
